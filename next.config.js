9edd71b4bff69fa3ef4694fdbf0937b0bb935aa4
Alex Gay <alexgay27753@gmail.com>
2023-10-13 12:12:53 +0800

    solved commit

--- next.config.js
+++ next.config.js
@@ -3,11 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = { 
-  reactStrictMode: true, 
-  eslint: { 
-    ignoreDuringBuilds: true, 
-  }, 
-}
-
  module.exports = nextConfig
